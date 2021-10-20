6f4ca326375f4c37c41e68b223b71b5b72740187
kksumm123 <kksumm123@gmail.com>
2021-10-20 19:33:55 +0900

    작성 완료

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    void Start()
+    public enum MoveDirection
     {
-        
+        Left,
+        Right,
     }
+    MoveDirection moveDirection = MoveDirection.Right;
 
     void Update()
     {
-        
+        ChangeMoveDirection();
+        Move();
+    }
+
+    private void ChangeMoveDirection()
+    {
+        if (Input.anyKeyDown)
+            moveDirection = moveDirection == MoveDirection.Left ? MoveDirection.Right : MoveDirection.Left;
+    }
+
+    Vector3 move = Vector3.zero;
+    [SerializeField] float speed = 3;
+    private void Move()
+    {
+        switch (moveDirection)
+        {
+            case MoveDirection.Left:
+                move = Vector3.forward;
+                break;
+            case MoveDirection.Right:
+                move = Vector3.right;
+                break;
+        }
+        transform.Translate(speed * Time.deltaTime * move, Space.Self);
     }
 }
